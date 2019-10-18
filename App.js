8efef3328a4e5d5e00adaaa5ef02b2b0b841e3c6
Guilherme Faleiros <guilhermefaleiros2000@gmail.com>
2019-10-18 17:01:12 -0300

    Commit 3

--- App.js
+++ App.js
@@ -25,6 +25,7 @@ export default class extends Component{
           <ImageBackground style={{height: 70, width: 70, alignItems: 'center', justifyContent: 'center'}}
                 source={require('./src/imgs/play-fundo.png')}>
                  {this.state.isPlaying  ? <Icon name="pause" size={30} color="gray"/> : <Image style={{marginLeft:5}}source={require('./src/imgs/play.png')}/>}
+                 <Text>oLÁ</Text>
           </ImageBackground>
         </View>
         </TWF>
